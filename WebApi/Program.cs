1b9d2c1d7fb7d8d676dad4a046344b61e6dd453a
Marco Viana <marcoviana1610@gmail.com>
2024-01-05 06:23:28 -0300

    JWT configuration

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,7 +9,10 @@ using Entities.Entities;
 using Infra.Configuration;
 using Infra.Repository;
 using Infra.Repository.Generics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,6 +46,39 @@ builder.Services.AddSingleton<IUsuarioSistemaFinanceiroService, UsuarioSistemaFi
 
 
 
+// CONFIGURAÇÃO JsonWebToken
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(option =>
+             {
+                 option.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+
+                     ValidIssuer = "Teste.Securiry.Bearer",
+                     ValidAudience = "Teste.Securiry.Bearer",
+                     IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+                 };
+
+                 option.Events = new JwtBearerEvents
+                 {
+                     OnAuthenticationFailed = context =>
+                     {
+                         Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                         return Task.CompletedTask;
+                     },
+                     OnTokenValidated = context =>
+                     {
+                         Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                         return Task.CompletedTask;
+                     }
+                 };
+             });
+
+
+
 
 var app = builder.Build();
 
@@ -54,6 +90,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//JWToken
+app.UseAuthentication();
 
 app.UseAuthorization();
 
