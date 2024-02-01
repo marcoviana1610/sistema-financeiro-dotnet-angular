9084c3e123e6ca2b39fa3c905cebaf0e7c8b0833
Marco Viana <marcoviana1610@gmail.com>
2024-02-01 12:23:46 -0300

    controllers, tests api

---
+++ WebApi/Controllers/SistemaFinanceiroController.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Interfaces.InterfaceServicos;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class SistemaFinanceiroController : ControllerBase
+    {
+        private readonly InterfaceSistemaFinanceiro _interfaceSistemaFinanceiro;
+        private readonly ISistemaFinanceiroService _iSistemaFinanceiroService;
+
+        public SistemaFinanceiroController(InterfaceSistemaFinanceiro interfaceSistemaFinanceiro,
+            ISistemaFinanceiroService iSistemaFinanceiroService)
+        {
+            _interfaceSistemaFinanceiro = interfaceSistemaFinanceiro;
+            _iSistemaFinanceiroService = iSistemaFinanceiroService;
+        }
+
+        [HttpGet("api/ListaSistemasUsuario")]
+        [Produces("application/json")]  
+        public async Task<object> ListaSistemasUsuario(string emailUsuario)
+        {
+            return await _interfaceSistemaFinanceiro.ListaSistemasUsuario(emailUsuario);
+        }
+
+        [HttpPost("api/AdicionarSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarSistemaFinanceiro(SistemaFinanceiro sistemaFinanceiro)
+        {
+            await _iSistemaFinanceiroService.AdicionarSistemaFinanceiro(sistemaFinanceiro);
+
+            return Task.FromResult(sistemaFinanceiro);
+        }
+        
+        [HttpPut("api/AtualizarSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> AtualizarSistemaFinanceiro(SistemaFinanceiro sistemaFinanceiro)
+        {
+            await _iSistemaFinanceiroService.AtualizarSistemaFinanceiro(sistemaFinanceiro);
+
+            return Task.FromResult(sistemaFinanceiro);
+        }
+
+        [HttpGet("api/ObterSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> ObterSistemaFinanceiro(int id)
+        {
+          return await _interfaceSistemaFinanceiro.GetEntityById(id);
+
+        }
+
+        [HttpDelete("api/DeleteSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> DeleteSistemaFinanceiro(int id)
+        {
+            var SistemaFinanceiro = await _interfaceSistemaFinanceiro.GetEntityById(id);
+
+            try
+            {
+                await _interfaceSistemaFinanceiro.Delete(SistemaFinanceiro);
+            } 
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WebApi/Controllers/UsuarioSistemaFinanceiroController.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Interfaces.InterfaceServicos;
+using Domain.Interfaces.IUsuarioSistemaFinanceiro;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class UsuarioSistemaFinanceiroController : ControllerBase
+    {
+        private readonly InterfaceUsuarioSistemaFinanceiro _interfaceUsuarioSistemaFinanceiro;
+        private readonly IUsuarioSistemaFinanceiroService _iusuarioSistemaFinanceiroService;
+
+        public UsuarioSistemaFinanceiroController(InterfaceUsuarioSistemaFinanceiro interfaceUsuarioSistemaFinanceiro, IUsuarioSistemaFinanceiroService iusuarioSistemaFinanceiroService)
+        {
+            _interfaceUsuarioSistemaFinanceiro = interfaceUsuarioSistemaFinanceiro;
+            _iusuarioSistemaFinanceiroService = iusuarioSistemaFinanceiroService;
+        }
+
+        [HttpGet("api/ListarUsuariosSistema")]
+        [Produces("application/json")]
+        public async Task<object> ListarUsuariosSistema(int idSistema)
+        {
+            return await _interfaceUsuarioSistemaFinanceiro.ListarUsuariosSistema(idSistema);
+        }
+
+        [HttpPost("api/CadastrarUsuarioNoSistema")]
+        [Produces("application/json")]
+        public async Task<object> CadastrarUsuarioNoSistema(int idSistema, string emailUsuario)
+        {
+            try
+            {
+                await _iusuarioSistemaFinanceiroService.CadastrarUsuarioNoSistema(new UsuarioSistemaFinanceiro
+                {
+                    IdSistema = idSistema,
+                    EmailUsuario = emailUsuario,
+                    Administrador = false,
+                    SistemaAtual = true,
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+
+
+            return Task.FromResult(true);
+        }
+
+        [HttpDelete("api/DeleteUsuarioSistemaFinanceiro")]
+        [Produces("application/json")]
+        public async Task<object> DeleteUsuarioSistemaFinanceiro(int id)
+        {
+            try
+            {
+
+                var usuarioSistemaFinanceiro = await _interfaceUsuarioSistemaFinanceiro.GetEntityById(id);
+
+                await _interfaceUsuarioSistemaFinanceiro.Delete(usuarioSistemaFinanceiro);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+
+
+            return Task.FromResult(true);
+        }
+
+    }
+}
