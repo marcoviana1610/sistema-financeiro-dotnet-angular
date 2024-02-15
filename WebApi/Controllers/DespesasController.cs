344b4f61425b26815baad5a9df629fcc3ca36fd1
Marco Viana <marcoviana1610@gmail.com>
2024-02-15 17:33:47 -0300

    a

---
+++ WebApi/Controllers/DespesasController.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServicos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DespesasController : ControllerBase
+    {
+        private readonly InterfaceDespesa _interfaceDespesa;
+        private readonly IDespesaService _despesaService;
+        public DespesasController(IDespesaService despesaService, InterfaceDespesa interfaceDespesa)
+        {
+            _despesaService = despesaService;
+            _interfaceDespesa = interfaceDespesa;
+        }
+    }
+}
